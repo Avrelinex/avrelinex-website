d2b450d3d322dd0ce4def32d4971baa3190f5cc7
Anton Lavrenchuk <anton@avrelinex.com>
2024-11-24 00:31:03 +0100

    footer

---
+++ app/components/AppFooter/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <header
+    class="bg-[#FAF7F5] w-full px-32 py-8 flex flex-row justify-between items-center"
+  >
+    <div class="flex flex-row gap-4 items-center">
+      <nuxt-icon name="logo" filled class="h-8" />
+      <span class="font-bold">&copy; 2024 Avrelinex sp. z o. o. All rights reserved.</span>
+    </div>
+    <a href="https://www.linkedin.com/company/avrelinex/">
+      <nuxt-icon name="linkedin" filled class="h-6" />
+    </a>
+  </header>
+</template>

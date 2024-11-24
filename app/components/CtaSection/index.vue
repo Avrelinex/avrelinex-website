7b5dd5049d643447bd67c9fec98827393ae9d319
Anton Lavrenchuk <anton@avrelinex.com>
2024-11-24 15:17:27 +0100

    gradient pulse

--- app/components/CtaSection/index.vue
+++ app/components/CtaSection/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-[#FAF7F5] section-container flex flex-row justify-center items-center gap-8 text-center md:text-left">
-    <div class="bg-gradient-to-r from-[#F26A1B] via-[#5A3D8C] via-35% to-[#041E3D] w-full flex flex-col jutify-center items-center px-8 py-5 md:px-32 md:py-12 rounded-[2rem] drop-shadow-2xl">
+    <div class="animated w-full flex flex-col jutify-center items-center px-8 py-5 md:px-32 md:py-12 rounded-[2rem] drop-shadow-2xl">
       <div class="flex flex-col jutify-center items-center">
         <span class="text-white font-medium text-[2.5rem] leading-normal">
           Let's Build Something Exceptional Together!
@@ -13,3 +13,24 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.animated {
+	background-size: 200% 200%;
+	animation: gradient 8s ease infinite;
+  background-image: linear-gradient(135deg, #F26A1B, #5A3D8C, #041E3D);
+}
+
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+</style>

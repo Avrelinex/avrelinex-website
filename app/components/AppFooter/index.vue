4e641ee9e54b9af9fdb45367f8b80cda16788410
anton-avrelinex <165930967+anton-avrelinex@users.noreply.github.com>
2025-02-15 11:01:20 +0100

    Update date

--- app/components/AppFooter/index.vue
+++ app/components/AppFooter/index.vue
@@ -4,7 +4,7 @@
   >
     <div class="flex flex-row md:gap-4 items-center">
       <nuxt-icon name="logo" filled class="h-8" />
-      <span class="font-bold text-sm text-center md:text-left md:text-base">&copy; 2024 Avrelinex sp. z o. o. All rights reserved.</span>
+      <span class="font-bold text-sm text-center md:text-left md:text-base">&copy; 2025 Avrelinex sp. z o. o. All rights reserved.</span>
     </div>
     <a href="https://www.linkedin.com/company/avrelinex/" target="_blank">
       <nuxt-icon name="linkedin" filled class="h-6" />
